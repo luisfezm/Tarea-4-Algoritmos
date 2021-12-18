dfb25bc0b01fd9835215beed81b1a2c92f74ce94
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-12-18 12:39:12 -0300

    nodoLista listo

--- src/NodoLista.java
+++ src/NodoLista.java
@@ -1,4 +1,21 @@
 
 public class NodoLista {
+	int key;
+	String item;
+	NodoLista nodoSiguiente;
+	NodoLista nodoAnterior;
+	
+	NodoLista(int key, String item){
+		this.key=key;
+		this.item=item;
+		nodoSiguiente=null;
+		nodoAnterior=null;
+	}
 
+	@Override
+	public String toString() {
+		return "[key=" + key + ", item=" + item + "]";
+	}
+	
+	
 }
